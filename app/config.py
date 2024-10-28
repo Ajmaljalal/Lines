706c9c522f64761922f8433d9f832ac0a7787101
ajmaljalal <ajmaljalal58@gmail.com>
2024-10-28 01:16:39 -0700

    preparing for heroku deployment

--- app/config.py
+++ app/config.py
@@ -7,3 +7,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+    ANTHROPIC_API_KEY = os.environ.get('ANTHROPIC_API_KEY')
+    TAVILY_API_KEY = os.environ.get('TAVILY_API_KEY')
+
